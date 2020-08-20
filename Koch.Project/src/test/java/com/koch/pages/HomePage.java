a427fc2f78403213bbe32ef7bfc8adcae8678070
rohittiwari7 <rohit.tiwari7@gmail.com>
2020-08-21 02:21:26 +0530

    Data Parameterization

--- Koch.Project/src/test/java/com/koch/pages/HomePage.java
+++ Koch.Project/src/test/java/com/koch/pages/HomePage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import com.koch.utility.ConfigDataProvider;
+
 public class HomePage
 {
 
@@ -17,13 +19,13 @@ public class HomePage
 	@FindBy(xpath = "//div[@class='form-fields']/div[5]/div/div/input") WebElement TravellersTab ;
 	@FindBy(xpath = "//*[@id='content']/div/div[1]/div[6]/div/div/div[5]/div/div[2]/div[2]/div[1]/div[2]/span[2]") WebElement NumberOfTravellers ;
 	@FindBy(xpath = "//div[@class='form-fields']/div[6]") WebElement SearchButton ;
-	
+	ConfigDataProvider config=new ConfigDataProvider();
 
 	 public void ValidateHomePage(WebDriver driver)
 	{ 
 		   String title=driver.getTitle();
 			//Validation of the landing page 
-			if(title.equalsIgnoreCase("ixigo - Flights, Train Reservation, Hotels, Air Tickets, Bus Booking"))
+			if(title.equalsIgnoreCase(config.getTitleHomePage()))
 			{
 				System.out.println("==============Landing Page validaiton done successfully=============================");
 			}
@@ -34,12 +36,12 @@ public class HomePage
 	 {
 	 //Source data 
 		     fromDefaultValue.click();
-		     From.sendKeys("Pune");
+		     From.sendKeys(config.getSource());
 		     Thread.sleep(2000);
 		     From.sendKeys(Keys.ENTER);
 		     
 		//Destination data
-		    To.sendKeys("Hyderabad");
+		    To.sendKeys(config.getdestination());
 		    Thread.sleep(2000);
 		    To.sendKeys(Keys.ENTER);
 		   
